0e73420bbf23b1602e0cb34f9b0ccc6c78bd131d
John Ellenberger <johne@jellenberger.org>
2013-02-24 11:20:20 -0500

    part 4

--- js/main.js
+++ js/main.js
@@ -20,12 +20,23 @@ var app = {
         alert(title ? (title + ": " + message) : message);
     }
 },
-	initialize: function() {
-		var self = this;
-		this.store = new MemoryStore(function() {
-			self.showAlert('Store Initialized', 'Info');
-		});
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+},
+
+initialize: function() {
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.renderHomeView();
+    });
 }
 
 };
