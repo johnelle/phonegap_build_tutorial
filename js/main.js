b7fe05f32a0d954ed8b95904b28067ca86c338a3
John Ellenberger <johne@jellenberger.org>
2013-02-24 11:05:42 -0500

    test native alerts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var app = {
-
-    findByName: function() {
+	
+	findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -12,11 +12,21 @@ var app = {
             }
         });
     },
-
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+	
+	showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
     }
+},
+	initialize: function() {
+		var self = this;
+		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
 
 };
 
